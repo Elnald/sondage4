64638a19c4e730fe8e80fa312d04cb341b3eedce
Elnald <yasminajib2008@gmail.com>
2024-06-26 19:44:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function vote(candidate) {
+    fetch('https://git.heroku.com/murmuring-citadel-94432.git', { // Remplace par ton URL publique ngrok
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ candidate: candidate }),
+    })
+    .then(response => response.text())
+    .then(message => alert(message))
+    .catch(error => console.error('Error:', error));
+}
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
